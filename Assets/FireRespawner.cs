12da315368249224a5970a57eaed7930a021cbd2
Patryk Marczak <m.patryk@outlook.com>
2023-04-19 00:15:42 +0200

    Resping collects

---
+++ Assets/FireRespawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireRespawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public Collect collect;
+    private BoxCollider2D collider;
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        StartCoroutine(RespNewCollect());
+    }
+
+    IEnumerator RespNewCollect()
+    {
+        while (true)
+        {
+            float randomX = Random.Range(-8.48f, 8.48f);
+            float randomY = Random.Range(-4.6f, 4.6f);
+            Instantiate(collect, new Vector3(randomX, randomY, -5), Quaternion.identity);
+            float randomSeconds = Random.Range(4, 8);
+            yield return new WaitForSeconds(randomSeconds);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
